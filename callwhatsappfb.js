b9584afd5f15cc145671b846849d12f11458fbe2
EnissayCula <77500416+EnissayCula@users.noreply.github.com>
2022-01-08 00:29:03 +0100

    Add files via upload

---
+++ callwhatsappfb.js
@@ -0,0 +1,14 @@
+(function () {
+        var options = {
+            facebook: "Facebook page ID", 
+            whatsapp: "WhatsApp number",
+            call_to_action: "يمكنك التواصل معنا", // Call to action
+            button_color: "#344275", // Color of button
+            position: "right", // Position may be 'right' or 'left'
+            order: "facebook,whatsapp", // Order of buttons
+        };
+        var proto = document.location.protocol, host = "whatshelp.io", url = proto + "//static." + host;
+        var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = url + '/widget-send-button/js/init.js';
+        s.onload = function () { WhWidgetSendButton.init(host, proto, options); };
+        var x = document.getElementsByTagName('script')[0]; x.parentNode.insertBefore(s, x);
+    })();
