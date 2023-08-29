5586710f44a885623b1bdbd0b7f855187f4cebf0
EnissayCula <77500416+EnissayCula@users.noreply.github.com>
2023-08-29 23:38:30 +0100

    Create all.cs

---
+++ styles/all.cs
@@ -0,0 +1,38 @@
+:root {
+    /* Fonts */
+    --Cairo: 'Cairo', sans-serif;
+    /* Colors */
+    --ColorBrown: rgb(148, 107, 71);
+    --BgBrownSm: blanchedalmond;
+    --ColorBlan: rgb(192, 181, 171);
+
+    /* Board*/
+    --ColorBWhite: rgba(255, 255, 255, 0.8);
+    --Colorbblack: black;
+
+    /* King */
+    --BgColorKing: #bd6a2f;
+    /* Player 1 */
+    --BgColorBiadqRD: #bd2f2f;
+    --bgColorBiadqRW: #fb7272;
+
+    /* Player 2*/
+    --BgColorBiadqBD: #2f68bd;
+    --BgColorBiadqBW: #7296fb;
+
+
+}
+
+@import url('https://fonts.googleapis.com/css2?family=Cairo:wght@200;300;400;500;600;700;800;900;1000&display=swap');
+
+* {
+    margin: 0;
+    padding: 0;
+    text-decoration: none;
+    font-family: var(--Cairo);
+}
+
+body {
+    background-color: var(--BgBrownSm);
+    color: var(--ColorBrown);
+}
