d1f5b0a16c07c1ea886fe8d478d3f7d0844b7137
EnissayCula <77500416+EnissayCula@users.noreply.github.com>
2023-08-29 23:45:47 +0100

    Create damas.js

---
+++ scripts/damas.js
@@ -0,0 +1,102 @@
+const damas = document.querySelector(".dama-container")
+
+damas.innerHTML = `
+                        <div class="grid-item point-black">
+                            <div class="Point-Dama"></div>
+                        </div>
+                        <div class="grid-item"></div>
+                        <div class="grid-item point-black">
+                            <div class="Point-Dama"></div>
+                        </div>  
+                        <div class="grid-item"></div>
+                        <div class="grid-item point-black"></div>
+                        <div class="grid-item"></div>  
+                        <div class="grid-item point-black">
+                            <p class="Point-Dama2"></p>
+                        </div>
+                        <div class="grid-item"></div>
+                        
+                        <div class="grid-item"></div>  
+                        <div class="grid-item point-black">
+                            <div class="Point-Dama"></div>
+                        </div>
+                        <div class="grid-item"></div>
+                        <div class="grid-item point-black"></div>
+                        <div class="grid-item"></div>
+                        <div class="grid-item point-black"><p class="Point-Dama2"></p></div>
+                        <div class="grid-item"></div>
+                        <div class="grid-item point-black"><p class="Point-Dama2"></p></div>
+                        
+                        <div class="grid-item point-black">
+                            <div class="Point-Dama"></div>
+                        </div>
+                        <div class="grid-item"></div>
+                        <div class="grid-item point-black">
+                            <div class="Point-Dama"></div>
+                        </div>  
+                        <div class="grid-item"></div>
+                        <div class="grid-item point-black"></div>
+                        <div class="grid-item"></div>  
+                        <div class="grid-item point-black"><p class="Point-Dama2"></p></div>
+                        <div class="grid-item"></div>
+                        
+                        <div class="grid-item"></div>  
+                        <div class="grid-item point-black">
+                            <div class="Point-Dama"></div>
+                        </div>
+                        <div class="grid-item"></div>
+                        <div class="grid-item point-black"></div>
+                        <div class="grid-item"></div>
+                        <div class="grid-item point-black"><p class="Point-Dama2"></p></div>
+                        <div class="grid-item"></div>
+                        <div class="grid-item point-black"><p class="Point-Dama2"></p></div>
+                        
+                        <div class="grid-item point-black">
+                            <div class="Point-Dama"></div>
+                        </div>
+                        <div class="grid-item"></div>
+                        <div class="grid-item point-black">
+                            <div class="Point-Dama"></div>
+                        </div>  
+                        <div class="grid-item"></div>
+                        <div class="grid-item point-black"></div>
+                        <div class="grid-item"></div>  
+                        <div class="grid-item point-black"><p class="Point-Dama2"></p></div>
+                        <div class="grid-item"></div>
+                        
+                        <div class="grid-item"></div>  
+                        <div class="grid-item point-black">
+                            <div class="Point-Dama"></div>
+                        </div>
+                        <div class="grid-item"></div>
+                        <div class="grid-item point-black"></div>
+                        <div class="grid-item"></div>
+                        <div class="grid-item point-black"><p class="Point-Dama2"></p></div>
+                        <div class="grid-item"></div>
+                        <div class="grid-item point-black"><p class="Point-Dama2"></p></div>
+                        
+                        <div class="grid-item point-black">
+                            <div class="Point-Dama"></div>
+                        </div>
+                        <div class="grid-item"></div>
+                        <div class="grid-item point-black">
+                            <div class="Point-Dama"></div>
+                        </div>  
+                        <div class="grid-item"></div>
+                        <div class="grid-item point-black"></div>
+                        <div class="grid-item"></div>  
+                        <div class="grid-item point-black"><p class="Point-Dama2"></p></div>
+                        <div class="grid-item"></div>
+                        
+                        <div class="grid-item"></div>  
+                        <div class="grid-item point-black">
+                            <div class="Point-Dama"></div>
+                        </div>
+                        <div class="grid-item"></div>
+                        <div class="grid-item point-black"></div>
+                        <div class="grid-item"></div>
+                        <div class="grid-item point-black"><p class="Point-Dama2"></p></div>
+                        <div class="grid-item"></div>
+                        <div class="grid-item point-black"><p class="Point-Dama2"></p></div>
+
+`
