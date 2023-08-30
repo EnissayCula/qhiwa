d70061c20914b8b402deff934c258b99f1cda549
EnissayCula <77500416+EnissayCula@users.noreply.github.com>
2023-08-30 03:45:42 +0100

    rey

--- scripts/Footer.js
+++
@@ -1,4 +0,0 @@
-const fonnter = document.querySelector(".Footer")
-fonnter.innerHTML = `
-Qhiwa by developers <a href="">Badr</a> and <a href="">YSN</a> © 2023
-`
--- scripts/header.js
+++
@@ -1,6 +0,0 @@
-const header = document.querySelector("#Header-Menu")
-
-header.innerHTML = `
-                    <li><a href="./en/lobby.html">Play Now!</a></li>
-                    
-`
