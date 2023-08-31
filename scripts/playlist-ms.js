c9472ba1d83dd585ed1fb581fc947c0ed14b5628
EnissayCula <77500416+EnissayCula@users.noreply.github.com>
2023-08-31 01:19:14 +0100

    Create playlist-ms.js

---
+++ scripts/playlist-ms.js
@@ -0,0 +1,102 @@
+ 
+const audioPlayer = document.getElementById('audio-player');
+const playlist = document.getElementById('playlist');
+const volumeInput = document.getElementById('volume-input');
+const backBtn = document.getElementById('back-btn');
+const pauseBtn = document.getElementById('pause-btn');
+const nextBtn = document.getElementById('next-btn');
+const shuffleBtn = document.getElementById('shuffle-btn');
+const progressBar = document.getElementById('progress-bar');
+const muteBtn = document.getElementById('mute-btn');
+
+
+const songs = [
+    { title: 'Song 1', source: 'song1.mp3' },
+    { title: 'Song 2', source: 'song2.mp3' },
+    { title: 'Song 3', source: 'song3.mp3' },
+    // Add more songs here
+];
+
+songs.forEach((song, index) => {
+    const listItem = document.createElement('li');
+    listItem.textContent = song.title;
+    listItem.addEventListener('click', () => {
+        audioPlayer.src = song.source;
+        audioPlayer.play();
+    });
+    playlist.appendChild(listItem);
+});
+// ... (same as before) ...
+
+
+
+let currentIndex = 0;
+
+function playSong(index) {
+    if (index >= 0 && index < songs.length) {
+        currentIndex = index;
+        const song = songs[currentIndex];
+        audioPlayer.src = song.source;
+        audioPlayer.play();
+    }
+}
+
+backBtn.addEventListener('click', () => {
+    currentIndex = (currentIndex - 1 + songs.length) % songs.length;
+    playSong(currentIndex);
+});
+
+pauseBtn.addEventListener('click', () => {
+    if (!audioPlayer.paused) {
+        audioPlayer.pause();
+    }
+});
+
+nextBtn.addEventListener('click', () => {
+    currentIndex = (currentIndex + 1) % songs.length;
+    playSong(currentIndex);
+});
+
+// Play the first song initially
+playSong(currentIndex);
+
+
+
+
+// Initialize volume to default value
+audioPlayer.volume = volumeInput.value;
+
+// Update volume when input changes
+volumeInput.addEventListener('input', () => {
+    audioPlayer.volume = volumeInput.value;
+});
+
+// ... (rest of the code, including button event listeners)
+
+// shuffle 
+
+
+shuffleBtn.addEventListener('click', () => {
+    currentIndex = Math.floor(Math.random() * songs.length);
+    playSong(currentIndex);
+});
+
+audioPlayer.addEventListener('timeupdate', () => {
+    const progressPercent = (audioPlayer.currentTime / audioPlayer.duration) * 100;
+    progressBar.style.width = `${progressPercent}%`;
+});
+
+// ... (same as before) ...
+
+muteBtn.addEventListener('click', () => {
+    if (audioPlayer.muted) {
+        audioPlayer.muted = false;
+        muteBtn.textContent = 'Mute';
+    } else {
+        audioPlayer.muted = true;
+        muteBtn.textContent = 'Unmute';
+    }
+});
+
+// ... (rest of the code)
+
