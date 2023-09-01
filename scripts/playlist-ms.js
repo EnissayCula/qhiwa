b760abb40573ecd9c3884dafeb48cb6a74e9c5df
EnissayCula <77500416+EnissayCula@users.noreply.github.com>
2023-09-01 16:46:12 +0100

    readd songs

--- scripts/playlist-ms.js
+++ scripts/playlist-ms.js
@@ -1,4 +1,3 @@
-import songs from '/qhiwa/scripts/songs.js';
 
 const audioPlayer = document.getElementById('audio-player');
 const playlist = document.getElementById('playlist');
@@ -10,6 +9,36 @@ const shuffleBtn = document.getElementById('shuffle-btn');
 const progressBar = document.getElementById('progress-bar');
 const muteBtn = document.getElementById('mute-btn');
 
+const songs = {
+    'Rock': [
+        {   
+            title: 'Pink Floyd - Marooned  low-q', 
+            source: './songes/Pink Floyd - Marooned  low-q.mp3' 
+        },
+        {   
+            title: 'Khaled - Wach Jabek Liy', 
+            source: './songes/Wach Jabek Liya.mp3' 
+        },
+        //add more
+    ],
+    'Pop': [
+        {   
+            title: 'The Weeknd - Cant Feel My Face', 
+            source: './songes/The Weeknd - Cant Feel My Face.mp3' 
+        },
+    
+        {   
+            title: 'The Weeknd - I Feel It Coming ft. Daft Punk', 
+            source: './songes/The Weeknd - I Feel It Coming ft. Daft Punk.mp3' 
+        },
+    
+        {   
+            title: 'The Weeknd - Save Your Tears', 
+            source: './songes/The Weeknd - Save Your Tears.mp3' 
+        },
+    ],
+    //add more albums
+};
 
 
 songs.forEach((song, index) => {
